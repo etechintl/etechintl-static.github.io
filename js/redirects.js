2debb10dab35978e2d837d64692c370afb2b9da4
Mahendra-Thapa <thapamadenhdra671@Email.com>
2025-06-27 12:37:28 +0545

    udated the code proeprly

--- js/redirects.js
+++ js/redirects.js
@@ -3,17 +3,22 @@
   let currentPath = window.location.pathname.toLowerCase().replace(/\/$/, '');
 
   // Treat root as /home
-  if (currentPath === '') currentPath = '/home';
+  if (currentPath === '') currentPath = '/home/';
 
   // Valid paths without trailing slash
   const validPaths = [
-    '/home',
-    '/about',
-    '/services',
-    '/team',
-    '/testimonials',
-    '/404',
-    '/contact',
+    '/home/',
+    '/about/',
+    '/services/',
+    '/our-product/',
+    '/team/',
+    '/blog/',
+    '/download/',
+    '/career/',
+    '/gallery/',
+    '/new-&-updates/',
+    '/404/',
+    '/contact/',
   ];
 
   if (!validPaths.includes(currentPath)) {
