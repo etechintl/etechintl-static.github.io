9b7570145ac209550e67f4c208a71c2b5cba02fa
Mahendra-Thapa <thapamadenhdra671@Email.com>
2025-07-03 15:13:47 +0545

    updated the code properly and while

--- js/redirects.js
+++ js/redirects.js
@@ -1,27 +1,27 @@
 (function () {
   // Normalize current path: lowercase and remove trailing slash
-  let currentPath = window.location.pathname.toLowerCase().replace(/\/$/, '');
+  let currentPath = window.location.pathname.toLowerCase().replace(/\/$/, "");
 
   // Treat root as /home
-  if (currentPath === '') currentPath = '/home/';
+  if (currentPath === "") currentPath = "/home/";
 
   // Valid paths without trailing slash
   const validPaths = [
-    '/home/',
-    '/about/',
-    '/services/',
-    '/our-product/',
-    '/team/',
-    '/blog/',
-    '/download/',
-    '/career/',
-    '/gallery/',
-    '/new-&-updates/',
-    '/404/',
-    '/contact/',
+    "/",
+    "/home/",
+    "/about/",
+    "/services/",
+    "/our-product/",
+    "/team/",
+    "/blog/",
+    "/download/",
+    "/career/",
+    "/gallery/",
+    "/news-&-updates/",
+    "/contact/",
   ];
 
   if (!validPaths.includes(currentPath)) {
-    window.location.href = '/404/';
+    window.location.href = "/404/";
   }
 })();
